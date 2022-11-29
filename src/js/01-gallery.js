5c30e6d96379f02173127de9c7c2f72140e934b3
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-11-29 18:31:32 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,4 @@
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightBox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 
