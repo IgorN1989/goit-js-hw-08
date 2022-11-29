6cb7ea335d874f796c44c0da2be092bca6abd8d1
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-11-29 18:03:48 +0200

    up

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,31 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log('hello');
+const optionsForLightbox = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+galleryRef.innerHTML = galleryMarkup;
+
+let lightbox = new SimpleLightbox('.gallery a', optionsForLightbox);
+
+function createGalleryMarkup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      loading="lazy"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`
+    )
+    .join('');
+}
