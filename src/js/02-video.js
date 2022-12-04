3a5a6d917e85c75b7ecb814ba7bef39a3be9ca1d
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-12-04 12:21:04 +0200

    aaaaaaaaaaaaa

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,8 @@
 import Player from '@vimeo/player';
 console.log(Player);
+
+const iframeRef = document.querySelector('#vimeo-player');
+
+console.log(
+  'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa'
+);
