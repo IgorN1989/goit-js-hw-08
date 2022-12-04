57f2391d320bb990b72a06d3c6b90ea480ea1dbd
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-12-04 13:49:18 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,24 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 console.log(Player);
 
-const iframeRef = document.querySelector('#vimeo-player');
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+const savedTime = localStorage.getItem(CURRENT_TIME);
+console.log(savedTime);
 
-console.log(
-  'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa'
-);
+player.on('timeupdate', throttle(onStartPlay, 1000));
+
+function onStartPlay(data) {
+  const currentPlayingTime = JSON.stringify(data.seconds);
+  localStorage.setItem(CURRENT_TIME, currentPlayingTime);
+
+  if (data.seconds === data.duration) {
+    localStorage.removeItem(CURRENT_TIME);
+  }
+}
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
