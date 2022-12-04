21228cc300475cde9d11fb97e155e9ae1369719a
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-12-04 13:50:03 +0200

    Update task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,10 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-console.log(Player);
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 const CURRENT_TIME = 'videoplayer-current-time';
 const savedTime = localStorage.getItem(CURRENT_TIME);
-console.log(savedTime);
 
 player.on('timeupdate', throttle(onStartPlay, 1000));
 
