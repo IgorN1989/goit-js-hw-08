da57f779ee26abf91412be8effb1913b6caeb9e6
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-12-05 01:12:56 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,47 @@
+import throttle from 'lodash.throttle';
+
 const formRef = document.querySelector('.feedback-form');
 const emailRef = formRef.querySelector('[name = "email"]');
 const messageRef = formRef.querySelector('[name = "message"]');
 const DATA_OBJECT_KEY = 'feedback-form-state';
-let savedData = {};
 
 inputDefaultValue();
 
-formRef.addEventListener('input', onInput);
+formRef.addEventListener('input', throttle(onInput, 500));
 formRef.addEventListener('submit', onSubmit);
 
-function onInput(evt) {
-  savedData[evt.target.name] = evt.target.value;
-  localStorage.setItem(DATA_OBJECT_KEY, JSON.stringify(savedData));
+function onInput() {
+  const email = emailRef.value;
+  const message = messageRef.value;
+  const savedData = {
+    email,
+    message,
+  };
+  const stringifiedSavedData = JSON.stringify(savedData);
+  localStorage.setItem(DATA_OBJECT_KEY, stringifiedSavedData);
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  if (emailRef.value.length < 1 || messageRef.value.length < 1) {
+    alert('Всі поля повинні бути заповнені!');
+    return;
+  } else {
+    let savedData = localStorage.getItem(DATA_OBJECT_KEY);
+    const parsedSavedData = JSON.parse(savedData);
+    console.log(parsedSavedData);
+    formRef.reset();
+    localStorage.clear();
+  }
 }
 
 function inputDefaultValue() {
   const defaultValue = localStorage.getItem(DATA_OBJECT_KEY);
   const parsedDefaultValue = JSON.parse(defaultValue);
+
   if (parsedDefaultValue) {
-    Object.entries(parsedDefaultValue).map(element => {
-      emailRef.value = parsedDefaultValue.email;
-      messageRef.value = parsedDefaultValue.message;
-    });
+    emailRef.value = parsedDefaultValue.email;
+    messageRef.value = parsedDefaultValue.message;
   }
-
-  console.log(defaultValue);
-  console.log(Object.entries(parsedDefaultValue));
 }
-
-function onSubmit(evt) {
-  console.log(evt);
-}
-
-// function startInputValue(email, message) {
-//   parsedSavedData = JSON.parse(savedData);
-//   email.value = localStorage.getItem(DATA_OBJECT_KEY);
-//   console.log(email);
-//   console.log(message);
-// }
-
-console.log('TEST');
